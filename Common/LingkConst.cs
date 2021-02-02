bd957147bebb571bb52f458abafc07aaa15fdc18
GT <gtalekar@infracloud.io>
2021-02-02 16:50:03 +0530

    added sample docker file

--- Common/LingkConst.cs
+++ Common/LingkConst.cs
@@ -10,7 +10,7 @@ namespace Lingk_SAML_Example.Common
         public static string DocusignReturnUrl = "https://localhost:3002?state=123";
         public static string TempSettingsPath = AppDomain.CurrentDomain.BaseDirectory + "./setting.json";
         public static string LingkFileSystemPath = AppDomain.CurrentDomain.BaseDirectory + "./lingkEnvelop.json";
-        public static string YamlConfigPath = "./example-docusign.yaml";
+        public static string YamlConfigPath =  AppDomain.CurrentDomain.BaseDirectory + "./example-docusign.yaml";
         public static string ClaimsUrl = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/";
 
         public static string DocusignAuthUrl = "https://account-d.docusign.com/oauth/token";
